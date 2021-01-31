05946c62fe8867447aa73cebbd40225ecf72e182
leandro-web <leandro-web@hotmail.com>
2021-01-31 14:09:09 -0300

    create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\{
+    PostController
+};
 use Illuminate\Support\Facades\Route;
+
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/', function () {
     return view('welcome');
